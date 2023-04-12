1db3b2d52dadc5f5303c46f73ff510283a6c4302
uujeen <rkddmlwls2@gmail.com>
2023-04-12 16:24:51 +0900

    [BKJ][GD4][9663] 주석 수정

--- Baekjoon/Gold/9663.py
+++ Baekjoon/Gold/9663.py
@@ -4,14 +4,14 @@ import sys
 def set(i):
     for j in range(N):
         global count
-        if (not flag_a[j] and not flag_b[i + j] and not flag_c[i - j + (N - 1)]):
+        if (not flag_a[j] and not flag_b[i + j] and not flag_c[i - j + (N - 1)]): 
             pos[i] = j
-            if i == N-1:
+            if i == N-1: # 해당 행을 다 채우면 count 1 증가
                 count += 1
             else:
-                flag_a[j] = flag_b[i + j] = flag_c[i - j + (N - 1)] = True
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + (N - 1)] = True # 배치 했기 떄문에 체크
                 set(i+1)
-                flag_a[j] = flag_b[i + j] = flag_c[i - j + (N - 1)] = False
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + (N - 1)] = False # 퀸을 j 행에서 제거해야 하므로 False로 전환
                 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
