718c67691174490f2861a347e9d4504c45b8710f
uujeen <rkddmlwls2@gmail.com>
2023-04-17 22:14:53 +0900

    [BKJ][SV4][10828]

---
+++ Baekjoon/Silver/10828.py
@@ -0,0 +1,45 @@
+import sys
+
+class Stack():
+    
+    def __init__(self):
+        self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+    
+    def pop(self):
+        try:
+            return self.items.pop()
+        except:
+            return -1
+
+    def __len__(self):
+        return len(self.items)
+
+    def empty(self):
+        if len(self.items) == 0:
+            return 1
+        else:
+            return 0
+    
+    def top(self):
+        try:
+            return self.items[-1]
+        except:
+            return -1
+
+N = int(sys.stdin.readline())
+s = Stack()
+for _ in range(N):
+    order = list(map(str, sys.stdin.readline().split()))
+    if order[0] == 'push':
+        s.push(int(order[1]))
+    elif order[0] == 'pop':
+        print(s.pop())
+    elif order[0] == 'size':
+        print(s.len())
+    elif order[0] == 'empty':
+        print(s.empty())
+    else:
+        print(s.top())
