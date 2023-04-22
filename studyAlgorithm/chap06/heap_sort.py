dc36c436e0b4bb50e92d3d72e905e3e94c81f68e
uujeen <rkddmlwls2@gmail.com>
2023-04-22 15:12:31 +0900

    [STY][Chap06][heap 주석 수정]

--- studyAlgorithm/chap06/heap_sort.py
+++ studyAlgorithm/chap06/heap_sort.py
@@ -15,11 +15,11 @@ def heapify(a: MutableSequence, left, right) -> None:
         
 def heap_sort(a: MutableSequence) -> None:
     n = len(a)
-    for i in range(n // 2, -1, -1): # 최대힙트리로 구성
+    for i in range(n // 2, -1, -1): # 최대힙트리로 정렬
         heapify(a, i, n)
-    for i in range(n - 1, -1, -1): # 정렬
-        a[0], a[i] = a[i], a[0]
-        heapify(a, 0, i)
+    # for i in range(n - 1, -1, -1): # 최소힙트리로 정렬
+    #     a[0], a[i] = a[i], a[0]
+    #     heapify(a, 0, i)
     return a
 
 if __name__ == '__main__':
