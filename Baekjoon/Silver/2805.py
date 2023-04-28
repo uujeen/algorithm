07f72598a57d4b38b4c495c6e567a9ec8fe00fd7
uujeen <rkddmlwls2@gmail.com>
2023-04-28 14:03:00 +0900

    [BOJ][SV2][2805][binarysearch_rewrite]

--- Baekjoon/Silver/2805.py
+++ Baekjoon/Silver/2805.py
@@ -1,19 +1,20 @@
 import sys
 """ 2805번 : 나무 자르기 (이분 탐색) """
-N, M = map(int, sys.stdin.readline().split())
+n, m = map(int, sys.stdin.readline().split())
 trees = list(map(int, sys.stdin.readline().split()))
-left, right = 1, max(trees)
+left, right = 0, max(trees)
 
-while left <= right:
+def check(mid, n, m, trees):
+    sum = 0
+    for i in range(n):
+        if trees[i] >= mid:
+            sum += trees[i] - mid
+    return sum >= m
+
+while left + 1 < right:
     mid = (left + right) // 2
-    sum_of_trees = 0
-
-    for i in trees:
-        if i >= mid:
-            sum_of_trees += i - mid
-
-    if sum_of_trees >= M:
-        left = mid + 1
+    if check(mid, n, m, trees):
+        left = mid
     else:
-        right = mid - 1
-print(right)
+        right = mid
+print(left)
