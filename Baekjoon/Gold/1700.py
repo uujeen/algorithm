9524560288e2068ff6a3a9dc2cb1d9751da5eab6
uujeen <rkddmlwls2@gmail.com>
2023-05-03 22:36:44 +0900

    [BOJ][GD1][1700][DP]

---
+++ Baekjoon/Gold/1700.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+items = list(map(int, input().split()))
+plugs = []
+count = 0
+for idx, item in enumerate(items):
+    if item in plugs:
+        continue
+    if len(plugs) < n:
+        plugs.append(item)
+        continue
+    else:
+        temp = []
+        for plug in plugs:
+            try:
+                i = items[idx:].index(plug)
+            except ValueError:
+                i = 101
+            temp.append(i)
+        select = temp.index(max(temp))
+        plugs[select] = items[idx]
+        count += 1
+print(count)
