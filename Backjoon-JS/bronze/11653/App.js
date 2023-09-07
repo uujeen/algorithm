a75a4fbbaf31dc399f2e1f37ee40150597703fb6
uujeen <rkddmlwls2@gmail.com>
2023-09-07 23:28:55 +0900

    [BOJ][BR1][11653]

--- Backjoon-JS/bronze/11653/App.js
+++ Backjoon-JS/bronze/11653/App.js
@@ -1,18 +1,19 @@
 let input = require('fs')
   .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
   .split('\n');
-input = Number(input);
+input = parseInt(input);
 let devide = 2;
-let arr = [];
+let arr = '';
+
 while (true) {
-  if (input === 1) {
-    return;
+  if (devide > input) {
+    break;
   }
   if (input % devide === 0) {
-    arr += devide + '\n';
     input = input / devide;
-  } else {
-    devide++;
+    arr += devide + '\n';
+    devide = 1;
   }
+  devide++;
 }
 console.log(arr);
