477dc301fc7d8f5a39fd715230ab66bfa76e2038
uujeen <rkddmlwls2@gmail.com>
2023-09-13 01:28:46 +0900

    [BOJ][SV5][10815][Set]

--- Backjoon-JS/silver/10815/App.js
+++ Backjoon-JS/silver/10815/App.js
@@ -5,29 +5,47 @@
  * for문 Dict 출력 N
  *
  */
+// let input = require('fs')
+//   .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
+//   .split('\n');
+// const n = parseInt(input[0]);
+// const myCard = [...input[1].split(' ')];
+// const m = parseInt(input[2]);
+// const checkCard = [...input[3].split(' ')];
+// const dict = {};
+// for (let i = 0; i < checkCard.length; i++) {
+//   let key = checkCard[i];
+//   dict[key] = -1;
+// }
+// for (let i = 0; i < myCard.length; i++) {
+//   if (dict[myCard[i]]) {
+//     dict[myCard[i]] = 1;
+//   }
+// }
+// const answer = [];
+// for (let i = 0; i < checkCard.length; i++) {
+//   if (dict[checkCard[i]] === -1) {
+//     answer.push(0);
+//   } else {
+//     answer.push(1);
+//   }
+// }
+// console.log(answer.join(' '));
+
+/**
+ * Set 사용
+ */
 let input = require('fs')
   .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
   .split('\n');
-const n = parseInt(input[0]);
-const myCard = [...input[1].split(' ')];
-const m = parseInt(input[2]);
+const myCard = new Set(input[1].split(' '));
 const checkCard = [...input[3].split(' ')];
-const dict = {};
+let answer = '';
 for (let i = 0; i < checkCard.length; i++) {
-  let key = checkCard[i];
-  dict[key] = -1;
-}
-for (let i = 0; i < myCard.length; i++) {
-  if (dict[myCard[i]]) {
-    dict[myCard[i]] = 1;
-  }
-}
-const answer = [];
-for (let i = 0; i < checkCard.length; i++) {
-  if (dict[checkCard[i]] === -1) {
-    answer.push(0);
+  if (myCard.has(checkCard[i])) {
+    answer += '1 ';
   } else {
-    answer.push(1);
+    answer += '0 ';
   }
 }
-console.log(answer.join(' '));
+console.log(answer);
