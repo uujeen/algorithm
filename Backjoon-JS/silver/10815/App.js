fe08080b559236c828f8eb889ac161b2545843b5
uujeen <rkddmlwls2@gmail.com>
2023-09-13 01:19:12 +0900

    [BOJ][SV5][10815][Dictionary]

---
+++ Backjoon-JS/silver/10815/App.js
@@ -0,0 +1,33 @@
+/**
+ * myCard
+ * checkCard Dict 형태로 저장
+ * for문 myCard N
+ * for문 Dict 출력 N
+ *
+ */
+let input = require('fs')
+  .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
+  .split('\n');
+const n = parseInt(input[0]);
+const myCard = [...input[1].split(' ')];
+const m = parseInt(input[2]);
+const checkCard = [...input[3].split(' ')];
+const dict = {};
+for (let i = 0; i < checkCard.length; i++) {
+  let key = checkCard[i];
+  dict[key] = -1;
+}
+for (let i = 0; i < myCard.length; i++) {
+  if (dict[myCard[i]]) {
+    dict[myCard[i]] = 1;
+  }
+}
+const answer = [];
+for (let i = 0; i < checkCard.length; i++) {
+  if (dict[checkCard[i]] === -1) {
+    answer.push(0);
+  } else {
+    answer.push(1);
+  }
+}
+console.log(answer.join(' '));
