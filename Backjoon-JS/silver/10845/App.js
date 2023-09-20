f5754357d3254cb2bcfcb5d8a5dd1f93faf160cf
uujeen <rkddmlwls2@gmail.com>
2023-09-20 17:01:19 +0900

    [BOJ][SV4][10845][Queue]

--- Backjoon-JS/silver/10845/App.js
+++ Backjoon-JS/silver/10845/App.js
@@ -1,3 +1,7 @@
+/**
+ * 연결 리스트로 구현
+ * value와 다음 노드를 가리킬 next
+ */
 class Node {
   constructor(value) {
     this.value = value;
@@ -5,36 +9,50 @@ class Node {
   }
 }
 
+/**
+ * queue의 head와 tail로 리스트의 구조를 정의
+ * size를 통해 queue의 길이를 저장
+ */
 class Queue {
   constructor() {
-    this.front = null;
-    this.rear = null;
+    this.head = null;
+    this.tail = null;
     this.size = 0;
   }
+  /**
+   * value를 받아 새로운 노드 생성
+   * queue가 비었을 경우 head와 tail에 해당 노드 저장
+   * 아닐 경우, queue의 tail을 갱신하고 size 증가
+   * @param {value} value
+   */
+  enqueue(value) {
+    const newNode = new Node(value);
 
-  enqueue(data) {
-    const newNode = new Node(data);
-
-    if (!this.front) {
-      this.front = newNode;
-      this.rear = newNode;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      this.rear.next = newNode;
-      this.rear = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
     this.size++;
   }
-
+  /**
+   * queue가 비었을 경우 -1반환
+   * 아닐 경우, head의 노드를 삭제하기 위해 저장 및 head를 head.next값으로 변경
+   * 만약 리스트가 하나의 노드만 존재했다면 tail에 null값을 갱신, size 감소
+   * @returns 삭제할 노드
+   */
   dequeue() {
-    if (!this.front) {
+    if (!this.head) {
       return -1;
     }
 
-    const removeNode = this.front;
-    this.front = this.front.next;
-    if (!this.front) {
-      this.rear = null;
+    const removeNode = this.head;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
     }
 
     this.size--;
@@ -53,18 +71,18 @@ class Queue {
     return 0;
   }
 
-  isFront() {
+  ishead() {
     if (this.size === 0) {
       return -1;
     }
-    return this.front.value;
+    return this.head.value;
   }
 
   isBack() {
     if (this.size === 0) {
       return -1;
     }
-    return this.rear.value;
+    return this.tail.value;
   }
 }
 
@@ -85,8 +103,8 @@ for (let i = 0; i < n; i++) {
     answer.push(q.len());
   } else if (order === 'empty') {
     answer.push(q.isEmpty());
-  } else if (order === 'front') {
-    answer.push(q.isFront());
+  } else if (order === 'head') {
+    answer.push(q.ishead());
   } else {
     answer.push(q.isBack());
   }
